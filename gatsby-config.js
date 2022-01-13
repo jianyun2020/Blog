bb144f62a1c6695528b4f113d93fc88ab2af51b9
jianyun2020 <sujianyun2020@163.com>
2022-01-13 23:54:40 +0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,9 @@
  */
 
  module.exports = {
+  siteMetadata: {
+    title: `Title from siteMetadata`
+  },
   plugins: [
     {
       resolve: `gatsby-plugin-typography`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react"
+import Layout from "../components/layout"
 
 const Home = () => (
-  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
-    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-    <p>
-      What do I like to do? Lots of course but definitely enjoy building
-      websites.
-    </p>
-  </div>
+  <Layout>
+    <h1>Amazing Pandas Eating Things</h1>
+    <div>
+      <img
+        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+        alt="Group of pandas eating bamboo"
+      />
+    </div>
+  </Layout>
 )
 
 export default Home
