023f13f2bd487600a995f2834489fef4ba43259e
jianyun2020 <sujianyun2020@163.com>
2022-01-14 19:55:40 +0800

    add

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,5 +11,12 @@
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`
+      }
+    }
   ],
 }
---
+++ src/pages/blog.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const BlogPage = () => (
+  <Layout pageTitle="My Blog Posts">
+  <p>My cool posts will go in here</p>
+</Layout>
+)
+
+export default BlogPage
