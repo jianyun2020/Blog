2feec6cdeb3767f656d7b73934b934d8b0861f26
jianyun2020 <sujianyun2020@163.com>
2022-01-14 20:09:38 +0800

    simple graphql

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,29 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-const BlogPage = () => (
+const BlogPage = ({ data }) => (
   <Layout pageTitle="My Blog Posts">
-  <p>My cool posts will go in here</p>
-</Layout>
+    <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+    </ul>
+  </Layout>
 )
+
+export const query = graphql`
+  query {
+    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export default BlogPage
