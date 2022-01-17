09d7061770c8767e66ad85f1edf51575012f34ec
jianyun2020 <sujianyun2020@163.com>
2022-01-17 20:48:15 +0800

    404

---
+++ src/404.js
@@ -0,0 +1,13 @@
+// Step 1: Import React
+import React from 'react'
+
+
+// Step 2: Define your component
+const NotFound = () => {
+  return (
+    <div>Not Found</div>
+  )
+}
+
+// Step 3: Export your component
+export default NotFound
