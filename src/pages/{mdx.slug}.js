d96e348e27bdb16258743def306463f6cc6ba112
jianyun2020 <sujianyun2020@163.com>
2022-02-22 18:29:42 +0800

    add

--- src/pages/{mdx.slug}.js
+++ src/pages/{mdx.slug}.js
@@ -3,36 +3,44 @@ import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
+import "./child.scss"
 
 const BlogPost = ({ data }) => {
   const image = getImage(data.mdx.frontmatter.image)
 
   return (
-    <div >
+    <div className="wrapper-child">
       <p>{data.mdx.frontmatter.Date}</p>
-      <GatsbyImage image={image} />
+      {/* <GatsbyImage image={image} /> */}
+      <div className="side-head">
+        {
+          data.mdx.tableOfContents.items.map(item => (
+            <a className="navigate-head" href={item.url}>{item.title}</a>
+          ))
+        }
+      </div>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </div>
   )
 }
 
 export const query = graphql`
-query ($id: String) {
-  mdx(id: {eq: $id}) {
-    frontmatter {
-      Date(formatString: "MMMM D, YYYY")
-      LastEditTime
-      type
-      image {
-        childImageSharp {
-          gatsbyImageData
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        Date(formatString: "MMMM D, YYYY")
+        LastEditTime
+        type
+        image {
+          childImageSharp {
+            gatsbyImageData
+          }
         }
       }
+      tableOfContents
+      body
     }
-    body
   }
-}
-
 `
 
 export default BlogPost
